b28ee24f4279c6939949bcc072174bb9d912430c
Eirini Marinou <EirMar@users.noreply.github.com>
2021-02-24 19:17:31 +0200

    Update Reverse_Time_Migration.py

--- Asteroid_model/Reverse_Time_Migration/Reverse_Time_Migration.py
+++ Asteroid_model/Reverse_Time_Migration/Reverse_Time_Migration.py
@@ -25,15 +25,17 @@ from salvus.flow import simple_config as config
 from salvus.toolbox import toolbox
 import salvus.namespace as sn
 import matplotlib.pyplot as plt
+import utils as ut
+from salvus.opt import smoothing
+
 
 # Variable used in the notebook to determine which site
 # is used to run the simulations.
-
 #SALVUS_FLOW_SITE_NAME=os.environ.get('SITE_NAME','local')
 SALVUS_FLOW_SITE_NAME=os.environ.get('eejit','eejit')
 
 
-
+# Import the model
 file = "vel1_copy.bin"
 dt = np.dtype([('time', '<u2'),('time1', '<u2'),('time2', np.float32),('time3', np.float32)])
 data = np.fromfile(file, dtype=np.float32, count=-1, sep='', offset=0)
@@ -41,7 +43,7 @@ data = np.fromfile(file, dtype=np.float32, count=-1, sep='', offset=0)
 my_array_rel_perm=data.reshape(3000,3000)
 my_array_rho=np.full((3000,3000),1000, dtype=int)
 
-
+# Define the model
 def my_model():
     nx, nz = 3000, 3000
     x = np.linspace(-4000, +4000, nx)
@@ -139,45 +141,42 @@ ec = sn.EventConfiguration(
     waveform_simulation_configuration=wsc,
     wavelet=sn.simple_config.stf.Ricker(center_frequency=10.0))
 
-# Simulation Configuration
-p += sn.SimulationConfiguration(
-    name="initial_model",
-    elements_per_wavelength=2,
-    tensor_order=2,
-    max_frequency_in_hertz = 20.0,
-    model_configuration=sn.ModelConfiguration(
-        background_model=None, volume_models="true_model"),
-    event_configuration=ec)
+# Simulation Configuration - UnstructuredMesh - smooth_model
+p += sn.UnstructuredMeshSimulationConfiguration(
+    name="smooth_model",
+    unstructured_mesh=smooth_model,
+    event_configuration=sn.EventConfiguration(
+        waveform_simulation_configuration=wsc,
+        wavelet=sn.simple_config.stf.Ricker(center_frequency=10.0)))
 
+# Simulation Configuration - Model - true_model
 p += sn.SimulationConfiguration(
     name="target_model",
     elements_per_wavelength=2,
     tensor_order=2,
     max_frequency_in_hertz = 20.0,
-    model_configuration=sn.ModelConfiguration(background_model="true_model"),
+    model_configuration=sn.ModelConfiguration(background_model=None, volume_models=["true_model"]),
     event_configuration=sn.EventConfiguration(
         waveform_simulation_configuration=wsc,
         wavelet=sn.simple_config.stf.Ricker(center_frequency=10.0)))
-
-p.simulations.get_mesh("initial_model")
 
 
 # %%
 # ------------------------------------------------------------------------------
 # RUN FORWARD SIMULATION
 # ------------------------------------------------------------------------------
-for sim, store in zip(["initial_model", "target_model"], [True, False]):
+for sim, store in zip(["smooth_model", "target_model"], [True, False]):
     p.simulations.launch(
         simulation_configuration=sim,
         events=p.events.get_all(),
         site_name=SALVUS_FLOW_SITE_NAME,
         ranks_per_job=48,
         verbosity=0,
-        store_adjoint_checkpoints=store,
+        store_adjoint_checkpoints=True,
         wall_time_in_seconds_per_job=1,)
 
 # %%
-for sim in ["initial_model", "target_model"]:
+for sim in ["smooth_model", "target_model"]:
     p.simulations.query(
         simulation_configuration=sim,
         events=p.events.list(),
@@ -205,11 +204,11 @@ p += sn.MisfitConfiguration(
 misfits = None
 while not misfits:
     misfits = p.actions.inversion.compute_misfits(
-        simulation_configuration="initial_model",
+        simulation_configuration="smooth_model",
         misfit_configuration="migration",
         store_checkpoints=False,
         events=p.events.list(),
-        ranks_per_job=4,
+        ranks_per_job=48,
         site_name=SALVUS_FLOW_SITE_NAME,
         wall_time_in_seconds_per_job=1,
         verbosity=2,
@@ -223,17 +222,17 @@ print(misfits)
 # RUN ADJOINT SIMULATION
 # ------------------------------------------------------------------------------
 p.simulations.launch_adjoint(
-    simulation_configuration="initial_model",
+    simulation_configuration="smooth_model",
     misfit_configuration="migration",
     events=p.events.list(),
     site_name=SALVUS_FLOW_SITE_NAME,
-    ranks_per_job=4,
+    ranks_per_job=48,
     wall_time_in_seconds_per_job=1,
     verbosity=True,)
 
 # %%
 p.simulations.query(
-    simulation_configuration="initial_model",
+    simulation_configuration="smooth_model",
     events=p.events.list(),
     misfit_configuration="migration",
     ping_interval_in_seconds=1.0,
@@ -242,7 +241,7 @@ p.simulations.query(
 
 # %%
 gradient = p.actions.inversion.sum_gradients(
-    simulation_configuration="initial_model",
+    simulation_configuration="smooth_model",
     misfit_configuration="migration",
     events=p.events.list(),)
 
@@ -253,33 +252,33 @@ gradient
 # ------------------------------------------------------------------------------
 # COLLECT WAVEFORM DATA
 # ------------------------------------------------------------------------------
-true_data = p.waveforms.get(
-    data_name="initial_model", events=p.events.get_all())
+#true_data = p.waveforms.get(
+#    data_name="initial_model", events=p.events.get_all())
 
-direct_wave = p.waveforms.get(
-    data_name="target_model", events=p.events.get_all())
+#direct_wave = p.waveforms.get(
+#    data_name="target_model", events=p.events.get_all())
 
-Rt = ut.get_gather(true_data)
-Rd = ut.get_gather(direct_wave)
-R = Rt - Rd
+#Rt = ut.get_gather(true_data)
+#Rd = ut.get_gather(direct_wave)
+#R = Rt - Rd
 
 # ------------------------------------------------------------------------------
 # PLOT SHOT GATHER
 # ------------------------------------------------------------------------------
 # Normalize and plot the shotgather.
-p_min, p_max = 0.001 * Rt.min(), 0.001 * Rt.max()
-ext = [500, 3500, 3, 0]
+#p_min, p_max = 0.001 * Rt.min(), 0.001 * Rt.max()
+#ext = [500, 3500, 3, 0]
 
-f, ax = plt.subplots(1, 3, figsize=(10, 8))
-ax[0].imshow(Rt[:, :, 10], vmin=p_min, vmax=p_max, extent=ext,
-             aspect="auto", cmap="gray")
-ax[1].imshow(Rd[:, :, 10], vmin=p_min, vmax=p_max, extent=ext,
-             aspect="auto", cmap="gray")
-ax[2].imshow(R[:, :, 10], vmin=p_min, vmax=p_max, extent=ext,
-             aspect="auto", cmap="gray")
-ax[0].set_title("P")
-ax[1].set_title("P direct")
-ax[2].set_title("P - direct wave removed")
+#f, ax = plt.subplots(1, 3, figsize=(10, 8))
+#ax[0].imshow(Rt[:, :, 10], vmin=p_min, vmax=p_max, extent=ext,
+#             aspect="auto", cmap="gray")
+#ax[1].imshow(Rd[:, :, 10], vmin=p_min, vmax=p_max, extent=ext,
+#             aspect="auto", cmap="gray")
+#ax[2].imshow(R[:, :, 10], vmin=p_min, vmax=p_max, extent=ext,
+#             aspect="auto", cmap="gray")
+#ax[0].set_title("P")
+#ax[1].set_title("P direct")
+#ax[2].set_title("P - direct wave removed")
 
 # %%
 
