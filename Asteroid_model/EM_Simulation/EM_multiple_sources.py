446af86e4fe0a61760a6551a70bcceb0dfc0cb29
davofis <davofis123@gmail.com>
2021-03-02 10:30:10 +0100

    Update the imports and give format to the script

--- Asteroid_model/EM_Simulation/EM_multiple_sources.py
+++ Asteroid_model/EM_Simulation/EM_multiple_sources.py
@@ -1,97 +1,81 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[18]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('config', 'Completer.use_jedi = False')
-
-
-# In[19]:
-
-
+# %%
 import os
-import numpy as np
-
-import xarray as xr
-
-from pathlib import Path 
-import salvus.namespace as sn
-from salvus.flow import simple_config as config
-
-import salvus.mesh.unstructured_mesh as um
-import salvus.mesh.structured_grid_2D as sg2d
-
-import salvus.namespace as sn
-
 import matplotlib.pyplot as plt
+from IPython import get_ipython
+import numpy as np
+import pathlib
 
-SALVUS_FLOW_SITE_NAME=os.environ.get('SITE_NAME','local')
+from salvus.toolbox import toolbox
+from salvus.flow import simple_config as config
+import salvus.namespace as sn
+
+from utils import my_model
+
+SALVUS_FLOW_SITE_NAME = os.environ.get('SITE_NAME', 'eejit')
+
+# %%
 
 
-# In[20]:
-
-
-#Import the model - Relative Permittivity values
+# Import the model - Relative Permittivity values
 
 file = "vel1_copy.bin"
 
-dt = np.dtype([('time', '<u2'),('time1', '<u2'),('time2', np.float32),('time3', np.float32)])
+dt = np.dtype([('time', '<u2'), ('time1', '<u2'),
+               ('time2', np.float32), ('time3', np.float32)])
 
 data = np.fromfile(file, dtype=np.float32, count=-1, sep='', offset=0)
 
-array_eps_rel = data.reshape (3000,3000) 
+array_eps_rel = data.reshape(3000, 3000)
 
 print(array_eps_rel)
-#print(my_array_eps_rel[1500,1500])
-#print(my_array_eps_rel[2999,2999])
-#print(my_array_eps_rel[1700,1700])
-#print(data)
-#type(data)
+# print(my_array_eps_rel[1500,1500])
+# print(my_array_eps_rel[2999,2999])
+# print(my_array_eps_rel[1700,1700])
+# print(data)
+# type(data)
 
 
 # In[21]:
 
 
-#Density
-array_rho = np.full((3000,3000),1000, dtype=int)
+# Density
+array_rho = np.full((3000, 3000), 1000, dtype=int)
 print(array_rho)
 
 
 # In[22]:
 
 
-#Magnetic Permeability
-array_mu = np.full((3000,3000),1, dtype=int)
+# Magnetic Permeability
+array_mu = np.full((3000, 3000), 1, dtype=int)
 print(array_mu)
 
 
 # In[23]:
 
 
-import math
-
-c = 3e8 #speed of light
+c = 3e8  # speed of light
 mu = 1
 #v_radar = c / math.sqrt(mu * array_eps_rel)
 
-array_eps_rel_sqrt=np.sqrt(array_eps_rel)
+array_eps_rel_sqrt = np.sqrt(array_eps_rel)
 print(array_eps_rel_sqrt)
 
 v_radar = c / (mu * array_eps_rel_sqrt)
 print(v_radar)
 
-#or
+# or
 
-#v_radar_1 = np.divide(c, array_eps_rel_sqrt) 
-#print(v_radar_1)
+#v_radar_1 = np.divide(c, array_eps_rel_sqrt)
+# print(v_radar_1)
 
 
 # In[24]:
 
 
-plt.imshow(np.rot90(v_radar,3))
+plt.imshow(np.rot90(v_radar, 3))
 plt.title('Asteroid model')
 plt.colorbar(orientation='vertical')
 plt.xlabel('x (m)')
@@ -99,10 +83,10 @@ plt.ylabel('y (m)')
 
 #plt.figure(figsize=(16, 6))
 
-#plt.subplot(121)
-#true_model.vp.T.plot()
-#plt.subplot(122)
-#true_model.rho.T.plot()
+# plt.subplot(121)
+# true_model.vp.T.plot()
+# plt.subplot(122)
+# true_model.rho.T.plot()
 
 plt.show()
 
@@ -114,12 +98,13 @@ def my_model():
     nx, nz = 3000, 3000
     x = np.linspace(-500, +500, nx)
     y = np.linspace(-500, +500, nx)
-    xx, yy = np.meshgrid(x, y, indexing = "ij")
-    
-    #put the array elements into the appropriate part of the model xarray structure
-    ds = xr.Dataset ( data_vars= {"vp": (["x", "y"], v_radar),  "rho": (["x", "y"], array_rho),}, coords={"x": x, "y": y},)
-    
-    #ds['vp'] *=10e9 #10e9
+    xx, yy = np.meshgrid(x, y, indexing="ij")
+
+    # put the array elements into the appropriate part of the model xarray structure
+    ds = xr.Dataset(data_vars={"vp": (["x", "y"], v_radar),  "rho": (
+        ["x", "y"], array_rho), }, coords={"x": x, "y": y},)
+
+    # ds['vp'] *=10e9 #10e9
     return ds
 
 
@@ -143,7 +128,6 @@ plt.show()
 # In[27]:
 
 
-import pathlib
 get_ipython().system('rm -rf project')
 if pathlib.Path("project").exists():
     print("Opening existing project.")
@@ -159,7 +143,7 @@ else:
 # In[28]:
 
 
-wavelet=sn.simple_config.stf.Ricker(center_frequency=15.0e6)
+wavelet = sn.simple_config.stf.Ricker(center_frequency=15.0e6)
 mesh_frequency = wavelet.center_frequency
 
 srcs = sn.simple_config.source.cartesian.collections.ScalarPoint2DRing(
@@ -168,16 +152,14 @@ srcs = sn.simple_config.source.cartesian.collections.ScalarPoint2DRing(
 
 
 recs = sn.simple_config.receiver.cartesian.collections.RingPoint2D(
-       x=0, y=0, radius=450, count=380, fields=["phi"]
+    x=0, y=0, radius=450, count=380, fields=["phi"]
 )
-    
+
 p += sn.EventCollection.from_sources(sources=srcs, receivers=recs)
 
 
 # In[29]:
 
-
-from salvus.toolbox import toolbox
 
 num_absorbing_layers = 10
 absorbing_side_sets = ["x0", "x1", "y0", "y1"]
@@ -188,7 +170,7 @@ mesh = toolbox.mesh_from_xarray(
     slowest_velocity='vp',
     maximum_frequency=mesh_frequency,
     elements_per_wavelength=2,
-    absorbing_boundaries=(absorbing_side_sets, num_absorbing_layers)) 
+    absorbing_boundaries=(absorbing_side_sets, num_absorbing_layers))
 
 
 # In[30]:
@@ -200,7 +182,7 @@ mesh
 # In[31]:
 
 
-sim = config.simulation.Waveform (mesh=mesh,sources=srcs,receivers=recs)
+sim = config.simulation.Waveform(mesh=mesh, sources=srcs, receivers=recs)
 
 
 # In[32]:
@@ -212,7 +194,7 @@ sim
 # In[33]:
 
 
-#s.physics.wave_equation.start_time_in_seconds = 
+# s.physics.wave_equation.start_time_in_seconds =
 #wsc = sn.WaveformSimulationConfiguration(start_time_in_seconds=1e-9)
 wsc = sn.WaveformSimulationConfiguration(end_time_in_seconds=9.5e-6)
 wsc.physics.wave_equation.time_step_in_seconds = 1.0e-10
@@ -279,7 +261,3 @@ true_data[0].plot(component="A", receiver_field="phi")
 
 
 # In[ ]:
-
-
-
-
