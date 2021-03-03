78ce6db0a36cdf1d4bdd7cc3e1308420e1b992ab
davofis <davofis123@gmail.com>
2021-03-03 14:02:28 +0100

    Update my_model

--- Asteroid_model/Reverse_Time_Migration/models.py
+++ Asteroid_model/Reverse_Time_Migration/models.py
@@ -2,17 +2,19 @@ import numpy as np
 import xarray as xr
 
 
-def my_model(vp, rho, nx, nz):
+def my_model(vp, rho, max_x, max_y):
     """
     """
-
-    x = np.linspace(-4000, +4000, nx)
-    y = np.linspace(-4000, +4000, nx)
+    nx, ny = vp.shape
+    x = np.linspace(-max_x, +max_x, nx)
+    y = np.linspace(-max_y, +max_y, nx)
     xx, yy = np.meshgrid(x, y, indexing="ij")
 
     # xarray dataset containing vp, rho
-    ds = xr.Dataset(data_vars={"vp": (["x", "y"], vp),  "rho": (
-        ["x", "y"], rho), }, coords={"x": x, "y": y},)
+    ds = xr.Dataset(
+        data_vars={"vp": (["x", "y"], vp),
+                   "rho": (["x", "y"], rho), },
+        coords={"x": x, "y": y},)
 
     # Transform velocity to SI units (m/s).
     ds['vp'] *= 10000
